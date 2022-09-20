ca367e4d54486263df8fede52c3274dbdf247e8c
josevictorsv <joseeviictor@gmail.com>
2022-09-20 04:06:29 -0300

    Department List

---
+++ src/GUI/DepartmentListController.java
@@ -0,0 +1,70 @@
+package GUI;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+import model.services.DepartmentService;
+
+public class DepartmentListController implements Initializable {
+	
+	private DepartmentService service;
+
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private Button btNew;
+	
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	private ObservableList<Department> obsList;
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+		
+	}
+
+	private void initializeNodes() {  					 //Padrao para iniciar comportamento das colunas
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty()); //macete para tamanho do tableview acompanhar a janela	
+	}
+
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);  //Instancia observable list com dados da lista
+		tableViewDepartment.setItems(obsList);
+	}
+}
