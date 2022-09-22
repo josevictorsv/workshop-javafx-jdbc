06a5420c8b8c928b8e722311565363327f2384c3
josevictorsv <joseeviictor@gmail.com>
2022-09-22 04:33:15 -0300

    Saving seller

--- src/GUI/util/Utils.java
+++ src/GUI/util/Utils.java
@@ -93,4 +93,11 @@ public class Utils {
 			}
 		});
 	}
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
